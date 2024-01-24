1abe00560085b20cf0f26451158a23c14db2dc93
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-24 14:09:54 +0100

    add new startup

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -2,13 +2,13 @@ return {
 	"startup-nvim/startup.nvim",
 	requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	config = function()
-		require("startup").setup({ theme = "Startify" })
-		vim.g.startup_bookmarks = {
-			["N"] = "~/.config/nvim",
-			["K"] = "~/.config/kitty/kitty.conf",
-			["S"] = "~/.skhdrc",
-			["Z"] = "~/.zshrc",
-			["Y"] = "~/.yabairc",
-		}
+		require("startup").setup({ theme = "Dashboard" })
+		-- vim.g.startup_bookmarks = {
+		-- 	["N"] = "~/.config/nvim",
+		-- 	["K"] = "~/.config/kitty/kitty.conf",
+		-- 	["S"] = "~/.skhdrc",
+		-- 	["Z"] = "~/.zshrc",
+		-- 	["Y"] = "~/.yabairc",
+		-- }
 	end,
 }
