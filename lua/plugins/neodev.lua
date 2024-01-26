0c109a16df355eabc8203c30f45e33d26795334e
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-26 15:18:27 +0100

    add neodev

---
+++ lua/plugins/neodev.lua
@@ -0,0 +1,9 @@
+return {
+	"folke/neodev.nvim",
+	opts = {},
+	config = function()
+		require("neodev").setup({
+			-- add any options here, or leave empty to use the default setting
+		})
+	end,
+}
