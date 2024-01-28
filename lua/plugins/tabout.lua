03e80dbeb8959c6e6186d4f7354d963cadcffcf3
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-28 22:42:30 +0100

    change load event

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -1,6 +1,6 @@
 return {
 	"abecodes/tabout.nvim",
-	event = { "BufRead", "BufNewFile" },
+	event = { "InsertEnter" },
 	config = function()
 		require("tabout").setup({
 			tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
