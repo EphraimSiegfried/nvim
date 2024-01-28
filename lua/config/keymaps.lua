c96493b2f741a05894abf8754cb9580709072bcc
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-28 22:42:05 +0100

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,12 +18,19 @@ end
 -- General Keymaps -------------------
 
 -- better up/down
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
+-- faster vertical movement
 map("n", "<C-u>", "<C-u>zz", {})
 map("n", "<C-d>", "<C-d>zz", {})
 map("n", "<C-b>", "<C-b>zz", {})
 map("n", "<C-f>", "<C-f>zz", {})
 
+-- Jump to previous buffer/position
 map("n", "<BS>", "<C-o>", {})
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,41 @@
 return {
 	"akinsho/toggleterm.nvim",
 
+	keys = {
+		{ "<C-x>", "<cmd>ToggleTerm<cr>", desc = "Toggle the terminal" },
+		{ "<leader>g", "<cmd>lua _lazygit_toggle()<cr>", desc = "Toggle the terminal" },
+	},
+
 	config = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			dir = "git_dir",
+			direction = "float",
+			float_opts = {
+				border = "double",
+			},
+			-- function to run on opening the terminal
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+			-- function to run on closing the terminal
+			on_close = function(term)
+				vim.cmd("startinsert!")
+			end,
+		})
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		-- function _G.set_terminal_keymaps()
+		-- 	local opts = { buffer = 0 }
+		-- 	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+		-- end
+		-- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 		require("toggleterm").setup({
 			size = function()
 				if vim.o.columns > 170 then
@@ -13,7 +47,7 @@ return {
 			direction = vim.o.columns > 170 and "vertical" or "horizontal",
 			shade_terminals = true,
 			insert_mappings = false,
-			open_mapping = [[<C-x>]],
+			open_mapping = [[<c-x>]],
 		})
 	end,
 }
