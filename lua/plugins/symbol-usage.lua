9955b51becb8568f03b391b210b07708701c477f
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-29 20:39:39 +0100

    fix setting

--- lua/plugins/symbol-usage.lua
+++ lua/plugins/symbol-usage.lua
@@ -2,6 +2,6 @@ return {
 	"Wansmer/symbol-usage.nvim",
 	event = "BufReadPre", -- need run before LspAttach if you use nvim -2.9. On 0.10 use 'LspAttach'
 	config = function()
-		require("symbol-usage").setup(opts)
+		require("symbol-usage").setup()
 	end,
 }
