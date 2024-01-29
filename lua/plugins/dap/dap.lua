936ab9aa780afa6b5f5f848e76d14865e72090ab
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-29 20:42:37 +0100

    comment out since doesn't work with import logic

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -1,30 +1,30 @@
 -- Adapter Plugins
-local cpptools = require("plugins.dap.adapters.cpptools")
-
--- Adapter Setups
-local debugpy = require("plugins.dap.adapters.debugpy")
+-- local cpptools = require("plugins.dap.adapters.cpptools")
+--
+-- -- Adapter Setups
+-- local debugpy = require("plugins.dap.adapters.debugpy")
 
 return {
 	"mfussenegger/nvim-dap",
-	keys = {
-		{
-			"<Leader>dc",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Start or continue debugging",
-		},
-		"<Leader>db",
-		function()
-			require("dap").toggle_breakpoint()
-		end,
-		desc = "Toggle Breakpoint",
-	},
-
-	debugpy.plugin(),
-
-	config = function()
-		require("dap")
-		cpptools.setup()
-	end,
+	-- keys = {
+	-- 	{
+	-- 		"<Leader>dc",
+	-- 		function()
+	-- 			require("dap").continue()
+	-- 		end,
+	-- 		desc = "Start or continue debugging",
+	-- 	},
+	-- 	"<Leader>db",
+	-- 	function()
+	-- 		require("dap").toggle_breakpoint()
+	-- 	end,
+	-- 	desc = "Toggle Breakpoint",
+	-- },
+	--
+	-- debugpy.plugin(),
+	--
+	-- config = function()
+	-- 	require("dap")
+	-- 	cpptools.setup()
+	-- end,
 }
