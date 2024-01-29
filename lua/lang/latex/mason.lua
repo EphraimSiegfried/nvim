849115c3db8165cc55e6dbdb7cd897cf103bcd02
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-29 20:41:06 +0100

    add latex config

---
+++ lua/lang/latex/mason.lua
@@ -0,0 +1,8 @@
+return {
+	"williamboman/mason.nvim",
+	opts = {
+		mason_null_ls = {
+			ensure_installed = { "latexindent" },
+		},
+	},
+}
---
+++ lua/lang/latex/plugins/latex-snippets.lua
@@ -0,0 +1,6 @@
+return {
+	"iurimateus/luasnip-latex-snippets.nvim",
+	ft = "tex",
+	dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
+	config = true,
+}
