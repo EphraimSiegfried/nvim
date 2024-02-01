66e35d40bb35202f91a2162ff2a0ea182bdb69e9
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-02-01 15:40:23 +0100

    add find keybinding keybinding

--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -10,6 +10,7 @@ return {
 		},
 		{ "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Find string in cwd" },
 		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+		{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Find Keybinding" },
 		{ "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
 		{ "<leader>fls", "<cmd>Telescope luasnip<cr>", desc = "Find Snippet" },
 	},
