aeebbc6ece1c06636114f4ae1f291c3bf7c65926
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-02-08 16:49:12 +0100

    enable debugger

--- lua/lang/java/mason.lua
+++ lua/lang/java/mason.lua
@@ -4,10 +4,11 @@ return {
 	opts = function(_, opts)
 		local lsp = opts.mason_lspconfig
 		-- local null_ls = opts.mason_null_ls
-		-- local dap = opts.mason_dap
+		local dap = opts.mason_dap
 		local extend = vim.list_extend
 
 		extend(lsp.ensure_installed, { "jdtls" })
+		extend(dap.ensure_installed, { "java-debug-adapter", "java-test" })
 		vim.tbl_extend("force", lsp.handlers, { jdtls = lsp_zero.noop }) -- ignore auto setup since nvim-jdtls will handle it
 	end,
 }
