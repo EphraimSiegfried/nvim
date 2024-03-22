fbdf234eb8f0488c6e08a6908697360450c4ef14
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-03-22 17:34:01 +0100

    update stuff

--- lua/plugins/ui/treesitter.lua
+++ lua/plugins/ui/treesitter.lua
@@ -1,28 +1,26 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		dependencies = {
-			"windwp/nvim-ts-autotag",
-		},
-		opts = {
-			ensure_installed = { "vim" },
-			auto_install = true,
-
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-			-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-			context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
-			},
-		},
-
-		config = function(_, opts)
-			-- opts.ensure_installed = vim.tbl_flatten(vim.tbl_values(opts.ensure_installed))
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
 	},
+	opts = {
+		ensure_installed = { "vim" },
+		auto_install = true,
+
+		highlight = { enable = true },
+		indent = { enable = true },
+		autotag = { enable = true },
+		-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+		context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+	},
+
+	config = function(_, opts)
+		-- opts.ensure_installed = vim.tbl_flatten(vim.tbl_values(opts.ensure_installed))
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
