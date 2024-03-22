a4d6f55d6c022f43eae7d0ebced66ece8da4c017
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-03-22 17:29:01 +0100

    add linter

--- lua/lang/bash/mason.lua
+++ lua/lang/bash/mason.lua
@@ -2,11 +2,12 @@ return {
 	"williamboman/mason.nvim",
 	opts = function(_, opts)
 		local lsp = opts.mason_lspconfig
-		-- local null_ls = opts.mason_null_ls
+		local null_ls = opts.mason_null_ls
 		local dap = opts.mason_dap
 		local extend = vim.list_extend
 
 		extend(lsp.ensure_installed, { "bashls" })
+		extend(null_ls.ensure_installed, { "shfmt" })
 		extend(dap.ensure_installed, { "bash" })
 	end,
 }
