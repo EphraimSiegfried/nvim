1f09e90d117ed456e910a6bc4eb3b12b0aaae88b
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-03-22 17:31:11 +0100

    fix settings

--- lua/lang/matlab/mason.lua
+++ lua/lang/matlab/mason.lua
@@ -7,20 +7,20 @@ return {
 		local extend = vim.list_extend
 
 		extend(lsp.ensure_installed, { "matlab_ls" })
-		-- vim.tbl_extend("force", lsp.handlers, {
-		-- 	matlab_ls = function()
-		-- 		require("lspconfig").matlab_ls.setup({
-		-- 			settings = {
-		-- 				matlab = {
-		-- 					capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		-- 					indexWorkspace = true,
-		-- 					installPath = "/Applications/MATLAB_R2023b.app",
-		-- 					telemetry = false,
-		-- 				},
-		-- 				single_file_support = true,
-		-- 			},
-		-- 		})
-		-- 	end,
-		-- })
+		vim.tbl_extend("force", lsp.handlers, {
+			matlab_ls = function()
+				require("lspconfig").matlab_ls.setup({
+					settings = {
+						matlab = {
+							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							indexWorkspace = true,
+							installPath = "/Applications/MATLAB_R2023b.app/bin/",
+							telemetry = false,
+						},
+						single_file_support = true,
+					},
+				})
+			end,
+		})
 	end,
 }
