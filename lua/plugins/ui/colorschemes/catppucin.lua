180662522f5b2d6dc3c8dbdbc3df6f647fda03a8
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-04-16 23:15:19 +0200

    add colorschemes

--- lua/plugins/ui/colorschemes/catppucin.lua
+++ lua/plugins/ui/colorschemes/catppucin.lua
@@ -1,5 +1,9 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	lazy = true,
+	priority = 1000, --make sure to load this before all other plugins
+	config = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme catppuccin-mocha]])
+	end,
 }
